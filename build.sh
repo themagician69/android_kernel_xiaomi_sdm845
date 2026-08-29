4ad8ec55bc329f295a9b9874128a274f0eae2c3a
themagician69 <sacrovano@gmail.com>
2026-08-30 06:29:34 +0700

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,79 +1,75 @@
-#!/bin/sh
+#!/bin/bash
 
 echo -e "*************************************************"
 echo -e "**                                             **"
-echo -e "** Building Etude-KSU via Standalone Clang     **"
-echo -e "** (Based on Nathan Chancellor's guidelines)   **"
+echo -e "** Building Etude-KSU via LineageOS GCC 4.9    **"
+echo -e "**                                             **"
 echo -e "*************************************************"
 
 # --- CLEAN PREVIOUS ARTIFACTS ---
 echo "Cleaning up previous build artifacts..."
 rm -rf out
 rm -rf AnyKernel3
-rm -rf clang
+rm -rf gcc64
+rm -rf gcc32
 rm -f *.zip
 # --------------------------------
 
-# KernelSU-Next
+# KernelSU-Next setup
 curl -LSs "https://raw.githubusercontent.com/rifsxd/KernelSU-Next/next/kernel/setup.sh" | bash -s v1.1.1
 
-# Some general variables
+# General variables
 KERNELNAME="Etude-Op.13-No.2-KSU-Next"
 ARCH="arm64"
 SUBARCH="arm64"
 DEVICE="beryllium"
 KERNEL_DIR="$(pwd)"
 
-# --- CLANG TOOLCHAIN SETUP ---
-echo "Downloading Prelude-Clang..."
-git clone -b master https://gitlab.com/jjpprrrr/prelude-clang.git --depth=1 clang
+# --- TOOLCHAIN SETUP (LineageOS GCC 4.9) ---
+echo "Downloading LineageOS AArch64 GCC 4.9..."
+git clone https://github.com/LineageOS/android_prebuilts_gcc_linux-x86_aarch64_aarch64-linux-android-4.9.git --depth=1 gcc64
 
-COMPILERDIR="${KERNEL_DIR}/clang"
-# -----------------------------
+echo "Downloading LineageOS ARM 32-bit GCC 4.9..."
+git clone https://github.com/LineageOS/android_prebuilts_gcc_linux-x86_arm_arm-linux-androideabi-4.9.git --depth=1 gcc32
 
-# --- OFFICIAL / STOCK MASKING OVERRIDES ---
+export PATH="${KERNEL_DIR}/gcc64/bin:${KERNEL_DIR}/gcc32/bin:${PATH}"
+# ------------------------------------------
+
+# Stock Masking Overrides
 rm -f localversion*
 touch localversion
 export KBUILD_BUILD_USER="root"
 export KBUILD_BUILD_HOST="localhost"
-# ------------------------------------------
 
-# Files
+# Files & Output
 IMAGE=$(pwd)/out/arch/arm64/boot/Image.gz-dtb
-
-# Clone AnyKernel
-echo "Cloning AnyKernel3"
+echo "Cloning AnyKernel3..."
 git clone --depth=1 https://github.com/Legendleo90/AnyKernel3.git AnyKernel3
 
 # Create Logs
 exec 2> >(tee -a out/error.log >&2)
 
-# Specify Final Zip Name
 ZIPNAME=Etude-KSU-Next-beryllium
 FINAL_ZIP=${ZIPNAME}-${DEVICE}.zip
 
-# Colors
 BUILD_START=$(date +"%s")
 yellow='\033[0;33m'
 nocol='\033[0m'
 
 # --- DEFCONFIG & MERGING STEP ---
 echo "Making base sdm845-perf_defconfig..."
-PATH="${COMPILERDIR}/bin:${PATH}" \
 make O=out ARCH=${ARCH} SUBARCH=${SUBARCH} sdm845-perf_defconfig
 
 if [ -f "arch/arm64/configs/vendor/xiaomi/beryllium.config" ]; then
     echo "Merging beryllium device-specific config..."
     ./scripts/kconfig/merge_config.sh -O out out/.config arch/arm64/configs/vendor/xiaomi/beryllium.config
-    PATH="${COMPILERDIR}/bin:${PATH}" \
     make O=out ARCH=${ARCH} SUBARCH=${SUBARCH} olddefconfig
 else
     echo "Error: arch/arm64/configs/vendor/xiaomi/beryllium.config not found!"
     exit 1
 fi
 
-if [ $? -ne 0 ]
-then
+if [ $? -ne 0 ]; then
     echo "Defconfig/Merge failed"
     exit 1
 else
@@ -81,45 +77,22 @@ else
 fi
 # --------------------------------
 
-# --- DISABLE BROKEN IOMMU DEBUG ---
-echo "Disabling CONFIG_IOMMU_DEBUG to fix struct dev_archdata errors..."
-if [ -f "out/.config" ]; then
-    sed -i 's/CONFIG_IOMMU_DEBUG=y/CONFIG_IOMMU_DEBUG=n/g' out/.config
-    echo "CONFIG_IOMMU_DEBUG=n" >> out/.config
-    echo "CONFIG_IOMMU_DEBUG_TRACKING=n" >> out/.config
-    PATH="${COMPILERDIR}/bin:${PATH}" make O=out ARCH=${ARCH} SUBARCH=${SUBARCH} olddefconfig
-fi
-# ----------------------------------
+# --- NATIVE GCC BUILD FUNCTION ---
+Build_GCC () {
+echo "Starting compilation using LineageOS GCC 4.9..."
 
-
-# --- NATHAN CHANCELLOR STANDALONE BUILD FUNCTION ---
-Build_Standalone_Clang () {
-echo "Starting compilation using Nathan Chancellor's standalone Clang variables..."
-
-PATH="${COMPILERDIR}/bin:${PATH}" \
 make -j$(nproc --all) O=out \
-ARCH=${ARCH} \
-SUBARCH=${SUBARCH} \
-CC=clang \
-CLANG_TRIPLE=aarch64-linux-gnu- \
-CROSS_COMPILE=aarch64-linux-gnu- \
-CROSS_COMPILE_ARM32=arm-linux-gnueabi- \
-LD=ld.lld \
-AR=llvm-ar \
-NM=llvm-nm \
-OBJCOPY=llvm-objcopy \
-OBJDUMP=llvm-objdump \
-STRIP=llvm-strip \
-KCFLAGS="-Wno-error=implicit-function-declaration -Wno-error=int-conversion" \
-KBUILD_COMPILER_STRING="Nathan Chancellor Standalone Clang"
+    ARCH=${ARCH} \
+    SUBARCH=${SUBARCH} \
+    CROSS_COMPILE=aarch64-linux-android- \
+    CROSS_COMPILE_ARM32=arm-linux-androideabi-
 }
-# ----------------------------------------------------
+# ---------------------------------
 
 # Run compilation
-Build_Standalone_Clang
+Build_GCC
 
-if [ $? -ne 0 ]
-then
+if [ $? -ne 0 ]; then
     echo "Build failed"
     exit 1
 else
@@ -128,10 +101,7 @@ fi
 
 ##----------------------------------------------------------------##
 zipping() {
-	# Copy Files To AnyKernel3 Zip
 	cp $IMAGE AnyKernel3/
-	
-	# Zipping and Push Kernel
 	cd AnyKernel3 || exit 1
     zip -r9 ${FINAL_ZIP} *
     MD5CHECK=$(md5sum "$FINAL_ZIP" | cut -d' ' -f1)
@@ -139,7 +109,6 @@ zipping() {
 }
 ##----------------------------------------------------------##
 
-# Run zipping function
 zipping
 
 BUILD_END=$(date +"%s")
