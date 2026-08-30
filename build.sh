18534e6bf408a9c0ee3f14d460c4b4f8f4515568
themagician69 <sacrovano@gmail.com>
2026-08-30 07:30:47 +0700

    Update build.sh

--- build.sh
+++ build.sh
@@ -102,7 +102,7 @@ print("compiler-gcc.h patched successfully.")
 # --- PATCH ADSPRPC ION DECLARATION ---
 echo "Patching drivers/char/adsprpc.c for ION import function..."
 if [ -f "drivers/char/adsprpc.c" ]; then
-    sed -i '/#include <linux\/msm_ion.h>/c\#include <linux\/msm_ion.h>\n#include "msm_ion.h"' drivers/char/adsprpc.c
+    sed -i '/#include <linux\/msm_ion.h>/c\#include <linux\/msm_ion.h>\n#include <drivers/staging/android/ion/msm/msm_ion.h>' drivers/char/adsprpc.c
 fi
 # -------------------------------------
 
